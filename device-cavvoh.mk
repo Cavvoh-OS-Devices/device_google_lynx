5d9e9ecae1e1eeb87b4d1b8ecd531e0ccb06dde0
Jonas <110699444+Cavvoh@users.noreply.github.com>
2024-10-19 12:42:54 +0200

    rebranding

---
+++ device-cavvoh.mk
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2024 The Cavvoh OS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-cavvoh
+
+# Udfps
+TARGET_HAS_UDFPS += true
